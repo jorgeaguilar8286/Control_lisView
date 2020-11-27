9b034f49321186753d597c941265a66835a36462
Jorge Aguilar <2014djjav@gmail.com>
2020-11-27 14:51:01 -0300

    control de listView terminado

---
+++ app/src/main/java/com/example/control_listview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.control_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+    private ListView lv1;
+
+    private String nombres [] = {"Jorge","Sigrid","Alejandra","Cristian","Alejandro","Miguel"};
+    private String edades [] = {"46","17","45","45","43","22"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+        lv1 = (ListView)findViewById(R.id.lv1);
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_djjav,nombres);
+        lv1.setAdapter(adapter);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tv1.setText ("La edad de " + lv1.getItemAtPosition(position) + " es " + edades [position] + " años " );
+            }
+        });
+    }
+}
